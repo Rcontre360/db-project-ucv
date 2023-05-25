01eb66719c791c830b4a6df6252fc919d8e112e9
Rcontre360 <rcontreraspimentel@gmail.com>
2023-05-24 21:41:58 -0400

    fix: multivalue syntax

--- sql/multivalue.sql
+++ sql/multivalue.sql
@@ -1,41 +1,48 @@
 
 CREATE TABLE ColoresTrajeHeroe(
-    NombreHeroe VARCHAR(255) PRIMARY KEY REFERENCES Personaje(NombreCompleto),
+    NombreHeroe VARCHAR(255),
     ColorTraje VARCHAR(255),
 
+    FOREIGN KEY (NombreHeroe) REFERENCES Personaje(NombreCompleto),
     PRIMARY KEY (NombreHeroe,ColorTraje)
 );
 
 CREATE TABLE EnemigoVillano(
-    NombreVillano VARCHAR(255) PRIMARY KEY REFERENCES Personaje(NombreCompleto),
+    NombreVillano VARCHAR(255),
     Enemigo VARCHAR(255),
 
+    FOREIGN KEY (NombreVillano) REFERENCES Personaje(NombreCompleto),
     PRIMARY KEY (NombreVillano,Enemigo)
 );
 
 CREATE TABLE OcupacionPersonaje(
-    NombrePersonaje VARCHAR(255) PRIMARY KEY REFERENCES Personaje(NombreCompleto),
+    NombrePersonaje VARCHAR(255),
     Ocupacion VARCHAR(255),
 
+    FOREIGN KEY (NombrePersonaje) REFERENCES Personaje(NombreCompleto),
     PRIMARY KEY (NombrePersonaje,Ocupacion)
 );
 
 CREATE TABLE CreadorPersonaje(
-    NombrePersonaje VARCHAR(255) PRIMARY KEY REFERENCES Personaje(NombreCompleto),
+    NombrePersonaje VARCHAR(255),
     Creador VARCHAR(255),
 
+    FOREIGN KEY (NombrePersonaje) REFERENCES Personaje(NombreCompleto),
     PRIMARY KEY (NombrePersonaje,Creador)
 );
 
 CREATE TABLE PlataformasVideojuego(
-    Videojuego VARCHAR(255) PRIMARY KEY REFERENCES Medio(TituloMedio)
+    Videojuego VARCHAR(255),
     Plataforma VARCHAR(255),
 
+    FOREIGN KEY (Videojuego) REFERENCES Medio(TituloMedio),
     PRIMARY KEY (Videojuego,Plataforma)
 );
 
 CREATE TABLE CreadorObjeto (
-    NombreObj VARCHAR(255) REFERENCES Objeto(NombreObj),
+    NombreObj VARCHAR(255),
     Creador VARCHAR(255),
+
+    FOREIGN KEY (NombreObj) REFERENCES Objeto(Nombre),
     PRIMARY KEY (NombreObj, Creador)
 );
